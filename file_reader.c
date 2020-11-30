f801d3cc7691e4eedc1582b20b6e042b3c8d1370
sebISU <Derendarz99@gmail.com>
2020-11-30 23:02:22 +0100

    enough for today

--- file_reader.c
+++ file_reader.c
@@ -3,78 +3,167 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <stdint.h>
+#include <time.h>
 #include "file_reader.h"
-
+#define SECTOR_SIZE 512
+#define VOLUMIN_SIGNATURE 0xAA55 // Value from resources, task author suggests 0x55AA BIG vs. LITTLE endian?
+#define LAST_CLUSTER(index) (index ^ 0xfff) // looking for the file's last cluster index
 
 
 struct disk_t* disk_open_from_file(const char* volume_file_name){
 
+    if (volume_file_name == NULL){
 
+        errno = EFAULT;
+        return NULL;
+    }
 
+    struct disk_t * disk = malloc(sizeof(struct disk_t));
+
+    if (disk == NULL){
+
+        errno = ENOMEM;
+        return NULL;
+    }
+
+    if((disk->file = fopen(volume_file_name, "rb")) == NULL){
+
+        errno = ENOENT;
+        return NULL;
+    }
+
+    fseek(disk->file, 0, SEEK_END);
+
+    disk->many_sectors = ftell(disk->file) / SECTOR_SIZE;   // what if will be 0?
+
+    return disk;
 }
 
 int disk_read(struct disk_t* pdisk, int32_t first_sector, void* buffer, int32_t sectors_to_read){
 
+    if (buffer == NULL || pdisk == NULL || pdisk->file == NULL){
 
+        errno = EFAULT;
+        return -1;
+    }
+    else if(first_sector < 0 || sectors_to_read < 0 || sectors_to_read > (int32_t)pdisk->many_sectors - first_sector){
 
+        errno = ERANGE;
+        return -1;
+    }
+
+    fread(buffer, SECTOR_SIZE, sectors_to_read, pdisk->file);
+
+    return sectors_to_read;
 }
 
 int disk_close(struct disk_t* pdisk){
 
+    if (pdisk == NULL || pdisk->file == NULL){
 
+        errno = EFAULT;
+        return -1;
+    }
 
+    fclose(pdisk->file);
+    free(pdisk);
+    pdisk = NULL;
+
+    return 0;
 }
 
 struct volume_t* fat_open(struct disk_t* pdisk, uint32_t first_sector){
 
+    if (pdisk == NULL){
+
+        errno = EFAULT;
+        return NULL;
+    }
+
+    struct volume_t * volumin = malloc(sizeof(struct volume_t));
+    struct extended_BPB * boot_sec = malloc(sizeof(struct extended_BPB));
+
+    if (volumin == NULL){
+
+        free(boot_sec);
+        errno = ENOMEM;
+        return NULL;
+    }
+    else if(boot_sec == NULL){
+
+        free(volumin);
+        errno = ENOMEM;
+        return NULL;
+    }
+
+    if (-1 == disk_read(pdisk, first_sector, boot_sec, 1)){
+
+        free(volumin);
+        free(boot_sec);
+        return NULL;
+    }
 
 
+
+
+
+    free(boot_sec);
+    return 0;
 }
 
 int fat_close(struct volume_t* pvolume){
 
+    if (pvolume == NULL){
 
+        errno = EFAULT;
+        return -1;
+    }
 
+    free(pvolume->clusters_indexes);
+    free(pvolume);
+    pvolume = NULL;
+
+    return 0;
 }
 
 struct file_t* file_open(struct volume_t* pvolume, const char* file_name){
 
 
-
+    return 0;
 }
 
 int file_close(struct file_t* stream){
 
 
-
+    return 0;
 }
 
 size_t file_read(void *ptr, size_t size, size_t nmemb, struct file_t *stream){
 
 
-
+    return 0;
 }
 
 int32_t file_seek(struct file_t* stream, int32_t offset, int whence){
 
 
-
+    return 0;
 }
 
 struct dir_t* dir_open(struct volume_t* pvolume, const char* dir_path){
 
 
-
+    return 0;
 }
 
 int dir_read(struct dir_t* pdir, struct dir_entry_t* pentry){
 
 
-
+    return 0;
 }
 
 int dir_close(struct dir_t* pdir){
 
 
-
+    return 0;
 }
--- file_reader.h
+++ file_reader.h
@@ -2,34 +2,122 @@
 #define F_READER_H
 
 #include <stdint.h>
+#include <time.h>
+
+struct BPB{
+    uint8_t assembly_inst[3];
+    uint64_t OEM;
+    uint16_t b_per_sect;
+    uint8_t sects_per_clust;
+    uint16_t reserved_sects;
+    uint8_t num_of_fats;
+    uint16_t max_num_of_root_dir;
+    uint16_t num_of_sects_in_fsys;
+    uint8_t media_type;
+    uint16_t sects_per_fat;
+    uint16_t sects_per_track;
+    uint16_t num_of_heads;
+    uint32_t num_of_sects_bef_start;
+    uint32_t sec_num_of_sects_in_fsys;
+} __attribute__((__packed__));
+
+struct extended_BPB{
+    struct BPB BPB_info;
+    uint8_t drive_number;
+    uint8_t not_used;
+    uint8_t sign_to_validate_next;
+    uint8_t vol_ser_num[4];
+    uint8_t vol_label[11];
+    uint64_t file_sys_type_lev;
+    uint8_t not_used_2[448];
+    uint16_t sign_value;
+} __attribute__((__packed__));
+
+union mod_cr_time{
+
+    uint16_t time;
+
+    struct{
+        uint16_t sec : 5;
+        uint16_t min : 6;
+        uint16_t hour : 5;
+    };
+};
+
+union mod_cr_date{
+
+    uint16_t date;
+
+    struct{
+        uint16_t day : 5;
+        uint16_t month : 4;
+        uint16_t year : 7;
+    };
+};
+
+union file_attrs{
+
+    uint8_t file_attributes;
+
+    struct{
+        uint8_t read_only : 1;
+        uint8_t hidden_file : 1;
+        uint8_t system_file : 1;
+        uint8_t volume_label : 1;
+        uint8_t directory : 1;
+        uint8_t archive : 1;
+        uint8_t bit7 : 1;
+        uint8_t bit8 : 1;
+    };
+};
+
+struct SFN{
+    char filename[8];
+    char ext[3];
+    union file_attrs file_attributes;
+    uint8_t reserved;
+    uint8_t file_creation_time;
+    union mod_cr_time creation_time;
+    union mod_cr_date creation_date;
+    uint16_t access_date;
+    uint16_t high_order_address_of_first_cluster;
+    union mod_cr_time modified_time;
+    union mod_cr_date modified_date;
+    uint16_t low_order_address_of_first_cluster;
+    uint32_t size;
+} __attribute__((__packed__));
 
 struct disk_t{
-
-
-
+    FILE * file;
+    uint32_t many_sectors;
 };
 
 struct volume_t{
 
-
-
+    uint32_t fat_size;
+    uint8_t many_fats;
+    uint32_t cluster_size;
+    uint32_t num_of_files_root_dir;
+    uint32_t root_dir_size;
+    uint16_t * clusters_indexes;
+    //add some fields to simplify moving during reading files
 };
 
 struct file_t{
 
-
+    int x;
 
 };
 
 struct dir_t{
 
-
+    int x;
 
 };
 
 struct dir_entry_t{
 
-
+    int name;
 
 };
 
